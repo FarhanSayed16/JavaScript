ae1bb3062432f8fc831f12868f7d1f4115bbb786
Farhan <farhansayed54@gmail.com>
2025-05-04 00:08:14 +0530

    New changes

--- test.js
+++ test.js
@@ -1,4 +1,25 @@
 console.log("Farhan")
-console.log("Farhan")
-console.log("Farhan")
-console.log("Farhan")
+
+const accountId = 145; //Non changeable
+let  accountEmail=  "Farhan@example.com"; //newly used 
+var accountPassword = "sim123"; // discarded and not in use since new js update
+accountCity = "Hyderabad" // This is i guess any type so anything can be stored in it 
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+//changed values
+// accountId = 5; // not allowed
+accountEmail = "Simranhu@huhu.com";
+accountPassword = 4541;
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+
+/*
+    Okay yeh bhi sikha ki numbers ko ya integervalues ko bina double inverted comas ke bina bhi likh sakte hai lekin string ko andar hi likhna hai
+    */
