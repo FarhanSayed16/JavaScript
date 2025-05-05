c7f46b46319ba8ba648733e487bbdfdcc81628c6
Farhan <farhansayed54@gmail.com>
2025-05-05 20:22:29 +0530

    new learning

---
+++ 02_dataTypes.js
@@ -0,0 +1,13 @@
+let farhan = null; //Null is treated as object in js || 
+let simran;
+console.log(typeof simran);
+console.log(typeof farhan);
+
+kaustubh = 523
+mymarks=(typeof "number");
+console.log(mymarks);
+
+Amiwinninfcurrently = false;
+console.log( typeof Amiwinninfcurrently, Amiwinninfcurrently);
+
+console.table([farhan,simran,kaustubh,Amiwinninfcurrently])
