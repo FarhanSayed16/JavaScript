714d27148538d66706e5a0da0301ac7a8bd97c1b
Farhan <farhansayed54@gmail.com>
2025-05-05 23:32:58 +0530

    New changes

---
+++ 03_1_dataOperations.js
@@ -0,0 +1,17 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2+2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+
+console.log("farhan"+ " Simran");
+let str = "Farhan";
+let str2 = "Simran";
+console.log(str + " " + str2);
+
+
+
+
+
