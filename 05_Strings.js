07d936b3b94f0ef306a65261b4984ca8361a5ad7
Farhan <farhansayed54@gmail.com>
2025-05-06 18:10:40 +0530

    new changes

---
+++ 05_Strings.js
@@ -0,0 +1,26 @@
+let Farhan = 12
+console.log(`Hello World! ${Farhan} this is trial`);
+
+const MyString = new String("Hello World!")
+console.log(MyString);
+console.log(MyString.length);
+console.log(MyString.toUpperCase());
+console.log(MyString.toLowerCase());
+console.log(MyString.charAt(0));
+console.log(MyString.charAt(1));
+console.log(MyString.indexOf("H"));
+console.log(MyString.indexOf("o","o"));
+
+const SubString = MyString.substring(0,5);
+console.log(SubString);
+
+const myurl = new URL("https://www.google.com/");
+console.log(myurl);
+console.log(myurl.host);
+console.log(myurl.href);
+console.log(myurl.searchParams);
+console.log(myurl.search)
+// console.log(myurl.replace("https","http"));
+
+
+// string and string method is the most powerfull and has many types, keep revising and learning this thing
