6e9cc6e2791fb3ecd01616427d761527463661c1
Farhan <farhansayed54@gmail.com>
2025-05-08 22:11:05 +0530

    Date and time

---
+++ 07_dateAndTime.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toUTCString());
+console.log(myDate.valueOf());
+console.log(myDate.getTime());
