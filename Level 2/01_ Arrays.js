0a4f35d169cff1e61fdcc03f152f125ee71260b8
Farhan <farhansayed54@gmail.com>
2025-05-10 17:35:08 +0530

    Slicing and Spicing

---
+++ Level 2/01_ Arrays.js
@@ -0,0 +1,42 @@
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(array);
+// console.log(array.length);
+// console.log(array[3]);
+// console.log(array.pop());
+// console.log(array);
+
+// console.log(array.push(11,12,123));
+// console.log(array);
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array);
+
+const myArray = new Array(45,46,47,48,49);
+// console.log(myArray)
+// console.log(myArray);
+// // console.log(myArray.length);
+// // console.log(myArray[3]);
+// // console.log(myArray.pop());
+// // console.log(myArray);
+// // console.log(myArray.push(11,12,123));
+// // console.log(myArray);
+// console.log(myArray.includes(2));
+// console.log(myArray.indexOf(5)) 
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1, 3)
+console.log("C ", myArray);
+console.log(myn2);
+
+// slicing and spicing ka topic acche se wapis samajna padega
+
+
